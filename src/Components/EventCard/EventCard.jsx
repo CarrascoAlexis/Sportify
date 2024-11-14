d4b09592a9798ab87233d3d3c1a617f543be4294
kroko_san <acarrasco@gaming.tech>
2024-11-14 16:41:16 +0100

    Ajout de la redirection vers la page dédiée à l'event

--- src/Components/EventCard/EventCard.jsx
+++ src/Components/EventCard/EventCard.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from 'react-router-dom'
 import './EventCard.css'
+
+
 
 export default function EventCard(props, key)
 {
     const event = props.event
+    
+    const navigate = useNavigate()
+    const test = () => {
+        navigate(`/events/${event.title}`)
+        return
+    }
     return(
-        <div className='event-card'>
+        <div className='event-card' onClick={() => test()}>
             <h3>{event.title}</h3>
         </div>
     )
